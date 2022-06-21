09bd461bf8561f24eef0ed96dbb2a192e0005b3c
benfrank241 <benjamenba@yahoo.ca>
2022-06-21 16:13:26 -0400

    Image Upload and Retrieve

--- kkskitchen/lib/screens/menu/dinner.dart
+++ kkskitchen/lib/screens/menu/dinner.dart
@@ -1,4 +1,7 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:kkskitchen/screens/storage/storage_service.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class Dinner extends StatefulWidget {
   const Dinner({Key? key}) : super(key: key);
@@ -10,7 +13,91 @@ class Dinner extends StatefulWidget {
 class _DinnerState extends State<Dinner> {
   @override
   Widget build(BuildContext context) {
+    final Storage storage = Storage();
     return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                final results = await FilePicker.platform.pickFiles(
+                  allowMultiple: false,
+                  type: FileType.custom,
+                  allowedExtensions: ['png', 'jpg'],
+                );
+
+                if (results == null) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('No file selected.'),
+                    ),
+                  );
+                  return;
+                }
+
+                final path = results.files.single.path!;
+                final fileName = results.files.single.name;
+
+                storage
+                    .uploadFile(path, fileName)
+                    .then((value) => print('Done'));
+              },
+              child: Text('Upload File'),
+            ),
+          ),
+          FutureBuilder(
+              future: storage.listFiles(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<firebase_storage.ListResult> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done &&
+                    snapshot.hasData) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    height: 50,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.items.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text(snapshot.data!.items[index].name),
+                            ),
+                        );
+                      })
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.waiting ||
+                    !snapshot.hasData) {
+                  return CircularProgressIndicator();
+                }
+                return Container();
+              }),
+              FutureBuilder(
+              future: storage.downloadURL('havana.jpg'),
+              builder: (BuildContext context,
+                  AsyncSnapshot<String> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done &&
+                    snapshot.hasData) {
+                  return  Container(
+                    width: 300,
+                    height: 250,
+                    child: Image.network(
+                      snapshot.data!,
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.waiting ||
+                    !snapshot.hasData) {
+                  return CircularProgressIndicator();
+                }
+                return Container();
+              })
+        ],
+      ),
     );
   }
 }
